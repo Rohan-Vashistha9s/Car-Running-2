afbfbbc0472c5471508119b3c3b34e98f8e66f70
rohan <panditrohan738@gmail.com>
2022-10-27 17:25:00 -0700

    reson

---
+++ script.js
@@ -0,0 +1,21 @@
+m=0;
+var y;
+function start(){
+     y=setInterval(run,1000);
+
+function run()
+{
+   if(m==1200){
+    clearInterval(y);
+    m+0;
+   }
+   else{
+    m+=50;
+    var x=document.getElementById('img');
+    x.style.marginLeft=m+'px';
+}
+}
+}
+function stop(){
+      clearInterval(y)
+}
